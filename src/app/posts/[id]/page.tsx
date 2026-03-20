927f56790bc6373bfa6ef5fc7fd1e0aea4d49179
o-jslove <gksflaxkdns12@gmail.com>
2026-03-20 12:51:27 +0900

    상세 페이지 수정

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,10 +1,22 @@
 'use client'
 
 import { useParams } from 'next/navigation'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function PostList() {
   const { id } = useParams()
   const [post, setPost] = useState({})
-  return <>{id}번 게시글 상세</>
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then((res) => res.json())
+      .then((res) => setPost(res))
+  }, [])
+  return (
+    <>
+      <div>{post.id}번 게시글 상세</div>
+      <div>{post.title}</div>
+      <div>{post.body}</div>
+    </>
+  )
 }
